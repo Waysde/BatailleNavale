8f2fb54b7dd948e1f87e33a1db48a80af8c86b5c
Waysde <101734726+Waysde@users.noreply.github.com>
2022-03-30 19:50:09 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -4,10 +4,10 @@ from Placement import recommencer
 
 class Game:
 
-    def __init__(self, width, height, taille, fps):
+    def __init__(self, width, fps):
         self.width = width
-        self.height = height
-        self.taille = taille
+        self.height = (width * 9)//16
+        self.taille = (width * 25)//64
         self.FPS = fps
         self.win = None
 
@@ -17,22 +17,34 @@ class Game:
         self.running = True
         self.changement = False
 
-        self.ecart_width = (width - taille) // 2
-        self.ecart_height = (height - taille) // 2
-        self.carre = taille // 10
-        rate = pygame.image.load("image/rate.png")
-        touche = pygame.image.load("image/explosion.png")
-        lancement = pygame.image.load("image/lancement.png")
-        placement = pygame.image.load("image/placement.png")
-        tir = pygame.image.load("image/tir.png")
-        fin = pygame.image.load("image/fin.png")
+        self.ecart_width = (width - self.taille) // 2
+        self.ecart_height = (self.height - self.taille) // 2
+        self.carre = self.taille // 10
 
-        self.images = [rate, touche, lancement, placement, tir, fin]
+        self.images = []
 
         self.joueur1 = {"boat": 0, "tour": True}
         self.joueur2 = {"boat": 0, "tour": False}
         self.gagnant = False
         self.perdant = False
+
+    def image(self):
+        rate = pygame.transform.scale(pygame.image.load("image/rate.png"), (self.carre, self.carre))
+        touche = pygame.transform.scale(pygame.image.load("image/explosion.png"), (self.carre, self.carre))
+        lancement = pygame.transform.scale(pygame.image.load("image/lancement.png"), (self.width, self.height))
+        placement = pygame.transform.scale(pygame.image.load("image/placement.png"), (self.width, self.height))
+        tir = pygame.transform.scale(pygame.image.load("image/tir.png"), (self.width, self.height))
+        fin = pygame.transform.scale(pygame.image.load("image/fin.png"), (self.width, self.height))
+        b5 = pygame.transform.scale(pygame.image.load("image/5 cases.png"), (5*self.carre, self.carre))
+        b4 = pygame.transform.scale(pygame.image.load("image/4 cases.png"), (4*self.carre, self.carre))
+        b3 = pygame.transform.scale(pygame.image.load("image/3 cases.png"), (3*self.carre, self.carre))
+        b2 = pygame.transform.scale(pygame.image.load("image/2 cases.png"), (2*self.carre, self.carre))
+        b5coule = pygame.transform.scale(pygame.image.load("image/5 casesCoule.png"), (5*self.carre, self.carre))
+        b4coule = pygame.transform.scale(pygame.image.load("image/4 casesCoule.png"), (4*self.carre, self.carre))
+        b3coule = pygame.transform.scale(pygame.image.load("image/3 casesCoule.png"), (3*self.carre, self.carre))
+        b2coule = pygame.transform.scale(pygame.image.load("image/2 casesCoule.png"), (2*self.carre, self.carre))
+
+        self.images = [rate, touche, b2coule, b3coule, b4coule, b5coule, lancement, placement, tir, fin, b2, b3, b4, b5]
 
     def init_valeur(self, armee1, armee2, plateau1, plateau2, win):
         self.joueur1["armee"] = armee1
@@ -57,13 +69,13 @@ class Game:
 
     def background(self):
         if self.debut:
-            self.win.blit(self.images[2], (0, 0))
+            self.win.blit(self.images[6], (0, 0))
         elif self.placement:
-            self.win.blit(self.images[3], (0, 0))
+            self.win.blit(self.images[7], (0, 0))
         elif self.tir:
-            self.win.blit(self.images[4], (0, 0))
+            self.win.blit(self.images[8], (0, 0))
         else:
-            self.win.blit(self.images[5], (0, 0))
+            self.win.blit(self.images[9], (0, 0))
 
     def debuter(self):
         for event in pygame.event.get():
